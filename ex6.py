e8e354ce755136a55d657056c78446658c8fec67
Joe Ryan <joe@clanryan.us>
2015-03-27 22:26:36 -0400

    first 10 exercises of python the hardway

---
+++ ex6.py
@@ -0,0 +1,4 @@
+shoplist = ["milk", "bread", "eggs"]
+
+print "Shopping list: %s %s %s, cost $%f" % (shoplist[0], shoplist[1], 
+                                             shoplist[2], 3.14)
---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something foine on here.
+With the three double quotes.
+We'll be able to type as much as we want.
+Even 4 lines if we want, or 5 or 6.
+"""
