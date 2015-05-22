8d99b65e928453ac1519c7bebf674d3b079432b2
Joe Ryan <joe@clanryan.us>
2015-05-21 21:29:14 -0400

    update ex16.py to show file before truncating

--- ex16.py
+++ ex16.py
@@ -3,19 +3,24 @@ from sys import argv
 
 script, filename = argv
 
+
+target = open(filename, 'r')
+file_contents = target.read()
+target.close()
+print "The file contains:"  
+print file_contents
+
 print "We're going to erase %r." % filename
 print "If you don't want that, hit Ctrl-C (^C)"
 print "If that is okay, hit RETURN."
 
 raw_input("?")
 
-print "Opening the file..."
 target = open(filename, 'w')
-
 print "Truncating the file.  Goodbye!"
+target.truncate()
 
 print "Now I'm going to ask for three lines."
-
 line1 = raw_input("line 1: ")
 line2 = raw_input("line 2: ")
 line3 = raw_input("line 3: ")
